008ce62819de02ab62322c5a956a5a2b84738a16
Charlize <chardebeer94@gmail.com>
2025-06-07 00:45:40 +0200

    fix: configure tailwind

---
+++ frontend/tailwind.config.mjs
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+const config = {
+  content: [
+    './pages/**/*.{js,jsx}',
+    './components/**/*.{js,jsx}',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
+
+export default config;
